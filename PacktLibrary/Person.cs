96b053d06a70488a0946fa9430f1f6591c204477
grety1016 <48903184+grety1016@users.noreply.github.com>
2020-06-03 11:56:05 +0800

    Initial commit

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Packt.Shared
+{
+    public class Person    
+    {
+        //fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> children = new List<Person>();
+
+        //methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        
+    }
+}
---
+++ PeopleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Packt.Shared
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sam = new Person(){Name = "Sam",DateOfBirth = new DateTime(1992,10,16)};
+            sam.WriteToConsole();
+            
+        }
+    }
+}
