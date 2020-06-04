9f97fc37e3a63446fce25a68bf7a19ad30ffd0fa
grety1016 <snl1016@163.com>
2020-06-04 10:10:10 +0800

    1

--- PacktLibrary/Person.cs
+++ PacktLibrary/Person.cs
@@ -9,12 +9,30 @@ namespace Packt.Shared
         //fields
         public string Name;
         public DateTime DateOfBirth;
-        public List<Person> children = new List<Person>();
+        public List<Person> Children = new List<Person>();
 
         //methods
         public void WriteToConsole()
         {
             WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        public static Person Procreate(Person p1,Person p2)
+        {
+            var baby = new Person()
+            {
+                Name  = $"Baby of {p1.Name} and {p2.Name}."
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this,partner);
         }
 
         
--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace Packt.Shared
 {
@@ -6,7 +8,18 @@ namespace Packt.Shared
     {
         static void Main(string[] args)
         {
-            
+            var harry = new Person { Name = "Harry"};
+            var mary = new Person { Name = "Mary"};
+            var jill = new Person { Name = "Jill"};
+            // call instance method
+            var baby1 = mary.ProcreateWith(harry);
+            // call static method
+            var baby2 = Person.Procreate(harry, jill) ;
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.") ;
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.") ;
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.") ;
+            WriteLine(format: "{0}'s first child is named \"{1}\". ",arg0: harry.Name,arg1: harry.Children[0].Name) ;
+                        
         }
     }
 }
