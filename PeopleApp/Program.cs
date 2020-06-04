bb39eecc620285ec85a8b98781da92e357fbdb1a
grety1016 <snl1016@163.com>
2020-06-04 10:11:45 +0800

    1

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -14,11 +14,11 @@ namespace Packt.Shared
             // call instance method
             var baby1 = mary.ProcreateWith(harry);
             // call static method
-            var baby2 = Person.Procreate(harry, jill) ;
-            WriteLine($"{harry.Name} has {harry.Children.Count} children.") ;
-            WriteLine($"{mary.Name} has {mary.Children.Count} children.") ;
-            WriteLine($"{jill.Name} has {jill.Children.Count} children.") ;
-            WriteLine(format: "{0}'s first child is named \"{1}\". ",arg0: harry.Name,arg1: harry.Children[0].Name) ;
+            var baby2 = Person.Procreate(harry, jill);
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine(format: "{0}'s first child is named \"{1}\". ",arg0: harry.Name,arg1: harry.Children[0].Name);
                         
         }
     }
